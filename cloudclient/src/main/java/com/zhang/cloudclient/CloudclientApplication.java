ba0f5cd953406ee1320636e1487b0fa9a66a3235
kylingongfu <fkoff@163.com>
2018-09-03 20:57:24 +0800

    '加入客户端项目'

---
+++ cloudclient/src/main/java/com/zhang/cloudclient/CloudclientApplication.java
@@ -0,0 +1,19 @@
+package com.zhang.cloudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudclientApplication.class, args);
+        try {
+            Thread.sleep(30*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

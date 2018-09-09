3cf616fc6501a1eae5cf7245a4e56059760546e1
kylingongfu <fkoff@163.com>
2018-09-09 22:35:17 +0800

    更多实现

--- cloud-eureka-client/src/main/java/com/zhang/product/dao/ProductDao.java
+++ cloud-eureka-client/src/main/java/com/zhang/product/dao/ProductDao.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface ProductDao extends JpaRepository<ProductInfo,String> {
     public List<ProductInfo> findAllByProductStatus(long status);
+
+    public List<ProductInfo> findByProductIdIn(List<String> productIds);
 }
